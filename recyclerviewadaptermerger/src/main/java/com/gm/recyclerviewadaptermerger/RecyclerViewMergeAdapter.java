0e894c7dc143989d06bb0fabb2f2f3782264a789
Gowtham <goutham.gm11@gmail.com>
2019-02-05 14:10:17 +0530

    Initial commit

---
+++ recyclerviewadaptermerger/src/main/java/com/gm/recyclerviewadaptermerger/RecyclerViewMergeAdapter.java
@@ -0,0 +1,344 @@
+package com.gm.recyclerviewadaptermerger;
+
+import android.util.SparseIntArray;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.collection.LongSparseArray;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Author     : Gowtham
+ * Email      : goutham.gm11@gmail.com
+ * Github     : https://github.com/goutham106
+ * Created on : 05/02/19.
+ */
+public class RecyclerViewMergeAdapter extends RecyclerView.Adapter {
+
+    private class AdapterDataObserver extends RecyclerView.AdapterDataObserver {
+
+        private RecyclerView.Adapter mAdapter;
+
+        AdapterDataObserver(RecyclerView.Adapter adapter) {
+            mAdapter = adapter;
+        }
+
+        @Override
+        public void onChanged() {
+            RecyclerViewMergeAdapter.this.notifyDataSetChanged();
+        }
+
+        @Override
+        public void onItemRangeChanged(int positionStart, int itemCount) {
+            int subAdapterOffset = getSubAdapterFirstGlobalPosition(mAdapter);
+            RecyclerViewMergeAdapter.this.notifyItemRangeChanged(subAdapterOffset + positionStart, itemCount);
+        }
+
+        @Override
+        public void onItemRangeInserted(int positionStart, int itemCount) {
+            int subAdapterOffset = getSubAdapterFirstGlobalPosition(mAdapter);
+            RecyclerViewMergeAdapter.this.notifyItemRangeInserted(subAdapterOffset + positionStart, itemCount);
+        }
+
+        @Override
+        public void onItemRangeMoved(int fromPosition, int toPosition, int itemCount) {
+            int subAdapterOffset = getSubAdapterFirstGlobalPosition(mAdapter);
+            RecyclerViewMergeAdapter.this.notifyItemMoved(subAdapterOffset + fromPosition, subAdapterOffset + toPosition);
+        }
+
+        @Override
+        public void onItemRangeRemoved(int positionStart, int itemCount) {
+            int subAdapterOffset = getSubAdapterFirstGlobalPosition(mAdapter);
+            RecyclerViewMergeAdapter.this.notifyItemRangeRemoved(subAdapterOffset + positionStart, itemCount);
+        }
+
+    }
+
+    /**
+     * LocalAdapter is a wrapper class that, for a given adapter, maintains a map of which indices in the global set of items refer to which view type(s).
+     */
+    public static class LocalAdapter {
+
+        public final RecyclerView.Adapter mAdapter;
+        public SparseIntArray mViewTypesMap = new SparseIntArray();
+        public AdapterDataObserver adapterDataObserver;
+        public LongSparseArray<Long> mItemIdMap = new LongSparseArray<>();
+
+        public LocalAdapter(@NonNull RecyclerView.Adapter adapter, @NonNull AdapterDataObserver adapterDataObserver) {
+            mAdapter = adapter;
+            this.adapterDataObserver = adapterDataObserver;
+        }
+
+    }
+
+    /**
+     * PosSubAdapterInfo is a wrapper class that holds a local position of an item and reference to the adapter it belongs to.
+     */
+    public static class PosSubAdapterInfo {
+
+        public final LocalAdapter localAdapter;
+        public final int posInSubAdapter;
+
+        public PosSubAdapterInfo(@NonNull LocalAdapter adapter, int position) {
+            localAdapter = adapter;
+            posInSubAdapter = position;
+        }
+
+        public RecyclerView.Adapter getAdapter() {
+            return localAdapter.mAdapter;
+        }
+
+        SparseIntArray getViewTypesMap() {
+            return localAdapter.mViewTypesMap;
+        }
+
+    }
+
+    protected List<LocalAdapter> mAdapters;
+    private int mViewTypeIndex;
+    private long mNextItemId;
+
+    public RecyclerViewMergeAdapter() {
+        mAdapters = new ArrayList<>();
+        mViewTypeIndex = 0;
+        mNextItemId = 0;
+    }
+
+    /**
+     * @return A List of LocalAdapter objects.
+     */
+    public List<LocalAdapter> getAdapters() {
+        return mAdapters;
+    }
+
+    /**
+     * @param adapter Append an adapter to the list of adapters.
+     */
+    public void addAdapter(@NonNull RecyclerView.Adapter adapter) {
+        addAdapter(mAdapters.size(), adapter);
+    }
+
+    /**
+     * @param index   The index at which to add an adapter to the list of adapters.
+     * @param adapter The adapter to add.
+     */
+    public void addAdapter(int index, @NonNull RecyclerView.Adapter adapter) {
+        AdapterDataObserver adapterDataObserver = new AdapterDataObserver(adapter);
+        mAdapters.add(index, new LocalAdapter(adapter, adapterDataObserver));
+        adapter.registerAdapterDataObserver(adapterDataObserver);
+    }
+
+    /**
+     * Clear all adapters from the list of adapters.
+     */
+    public void clearAdapters() {
+        for (LocalAdapter localAdapter : mAdapters) {
+            localAdapter.mAdapter.unregisterAdapterDataObserver(localAdapter.adapterDataObserver);
+        }
+
+        mAdapters.clear();
+    }
+
+    /**
+     * Check if a specified adapter has been added.
+     *
+     * @param adapter The adapter to check in the list of adapters.
+     * @return Whether the list of adapters contains the adapter.
+     */
+    public boolean containsAdapter(RecyclerView.Adapter adapter) {
+        for (LocalAdapter localAdapter : mAdapters) {
+            if (localAdapter.mAdapter.equals(adapter)) return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Remove a specific adapter from the list of adapters.
+     *
+     * @param adapter The adapter to remove from the list of adapters.
+     */
+    public void removeAdapter(RecyclerView.Adapter adapter) {
+        for (int i = mAdapters.size() - 1; i >= 0; i--) {
+            LocalAdapter local = mAdapters.get(i);
+
+            if (local.mAdapter.equals(adapter)) {
+                removeAdapter(mAdapters.indexOf(local));
+            }
+        }
+    }
+
+    /**
+     * Remove the adapter at a specific index from the list of adapters.
+     *
+     * @param index The index in the adapter list at which an adapter should be removed.
+     */
+    public void removeAdapter(int index) {
+        LocalAdapter localAdapter = mAdapters.get(index);
+        localAdapter.mAdapter.unregisterAdapterDataObserver(localAdapter.adapterDataObserver);
+        mAdapters.remove(localAdapter);
+    }
+
+    /**
+     * Return the number of adapters in the list of adapters.
+     *
+     * @return The number of adapters.
+     */
+    public int getSubAdapterCount() {
+        return mAdapters.size();
+    }
+
+    /**
+     * Get a specific adapter for a given index.
+     *
+     * @param index The index for which the return the adapter.
+     * @return The adapter which was found at the given index.
+     */
+    public RecyclerView.Adapter getSubAdapter(int index) {
+        return mAdapters.get(index).mAdapter;
+    }
+
+    /**
+     * Return a PosSubAdapterInfo object for a given global position.
+     *
+     * @param globalPosition The global position in the entire set of items.
+     * @return A PosSubAdapterInfo object containing a reference to the adapter and the local
+     * position in that adapter that corresponds to the given global position.
+     */
+    @Nullable
+    public PosSubAdapterInfo getPosSubAdapterInfoForGlobalPosition(final int globalPosition) {
+        final int adapterCount = mAdapters.size();
+
+        int i = 0;
+        int count = 0;
+
+        while (i < adapterCount) {
+            LocalAdapter a = mAdapters.get(i);
+            int newCount = count + a.mAdapter.getItemCount();
+
+            if (globalPosition < newCount) {
+                return new PosSubAdapterInfo(a, globalPosition - count);
+            }
+
+            count = newCount;
+            i++;
+        }
+
+        return null;
+    }
+
+    @Override
+    @Nullable
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+        for (LocalAdapter adapter : mAdapters) {
+            int localViewType = adapter.mViewTypesMap.get(viewType, -1);
+
+            if (localViewType >= 0) {
+                return adapter.mAdapter.onCreateViewHolder(viewGroup, localViewType);
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * Return the first global position in the entire set of items for a given adapter.
+     *
+     * @param adapter The adapter for which to the return the first global position.
+     * @return The first global position for the given adapter, or -1 if no such position could be found.
+     */
+    public int getSubAdapterFirstGlobalPosition(RecyclerView.Adapter adapter) {
+        int count = 0;
+
+        for (LocalAdapter localAdapter : mAdapters) {
+            RecyclerView.Adapter adapter_ = localAdapter.mAdapter;
+
+            if (adapter_.equals(adapter) && adapter_.getItemCount() > 0) {
+                return count;
+            }
+
+            count += adapter_.getItemCount();
+        }
+
+        return -1;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder viewHolder, int position) {
+        PosSubAdapterInfo posSubAdapterInfo = getPosSubAdapterInfoForGlobalPosition(position);
+        RecyclerView.Adapter adapter = posSubAdapterInfo.getAdapter();
+        adapter.onBindViewHolder(viewHolder, posSubAdapterInfo.posInSubAdapter);
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        PosSubAdapterInfo result = getPosSubAdapterInfoForGlobalPosition(position);
+        int localViewType = result.getAdapter().getItemViewType(result.posInSubAdapter);
+        int localViewTypeIndex = result.getViewTypesMap().indexOfValue(localViewType);
+
+        if (localViewTypeIndex >= 0) {
+            return result.getViewTypesMap().keyAt(localViewTypeIndex);
+        }
+
+        mViewTypeIndex += 1;
+        result.getViewTypesMap().append(mViewTypeIndex, localViewType);
+
+        return mViewTypeIndex;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        PosSubAdapterInfo posSubAdapterInfo = getPosSubAdapterInfoForGlobalPosition(position);
+        long localItemId = posSubAdapterInfo.getAdapter().getItemId(posSubAdapterInfo.posInSubAdapter);
+
+        if (RecyclerView.NO_ID == localItemId) {
+            return localItemId;
+        } else {
+            long globalItemId = posSubAdapterInfo.localAdapter.mItemIdMap.get(localItemId, RecyclerView.NO_ID);
+
+            if (RecyclerView.NO_ID == globalItemId) {
+                mNextItemId++;
+                globalItemId = mNextItemId;
+                posSubAdapterInfo.localAdapter.mItemIdMap.put(localItemId, globalItemId);
+            }
+
+            return globalItemId;
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        int count = 0;
+
+        for (LocalAdapter adapter : mAdapters) {
+            count += adapter.mAdapter.getItemCount();
+        }
+
+        return count;
+    }
+
+    /**
+     * Add one or more View objects to the adapter.
+     *
+     * @param view The View or View(s) to add to the adapter. Do not pass null into this method.
+     */
+    public void addView(@NonNull View... view) {
+        List<View> viewList = Arrays.asList(view);
+        addViews(viewList);
+    }
+
+    /**
+     * A List of View objects to the adapter at once.
+     *
+     * @param views The Views to add to the adapter. Do not pass null into this method.
+     */
+    public void addViews(@NonNull List<View> views) {
+        addAdapter(new ViewAdapter(views));
+    }
+
+}
---
+++ recyclerviewadaptermerger/src/main/java/com/gm/recyclerviewadaptermerger/ViewAdapter.java
@@ -0,0 +1,109 @@
+package com.gm.recyclerviewadaptermerger;
+
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Author     : Gowtham
+ * Email      : goutham.gm11@gmail.com
+ * Github     : https://github.com/goutham106
+ * Created on : 05/02/19.
+ */
+public class ViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    private final List<View> viewList = new ArrayList<>();
+
+    public ViewAdapter() {
+        this(Collections.<View>emptyList());
+    }
+
+    public ViewAdapter(@NonNull List<View> viewList) {
+        super();
+        this.viewList.addAll(viewList);
+    }
+
+    @Override
+    public int getItemCount() {
+        return viewList.size();
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return (position);
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+        // View type is the index in the list of views.
+        return new RecyclerView.ViewHolder(viewList.get(viewType)) {};
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder viewHolder, int position) {}
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    public void setViews(List<View> views) {
+        viewList.clear();
+
+        if (views != null) {
+            viewList.addAll(views);
+        }
+
+        notifyDataSetChanged();
+    }
+
+    public void clear() {
+        if (viewList != null) {
+            viewList.clear();
+        }
+
+        notifyDataSetChanged();
+    }
+
+    public void addView(int position, @NonNull View view) {
+        viewList.add(position, view);
+        notifyItemInserted(position);
+    }
+
+    public View removeView(int position) {
+        if (position < viewList.size()) {
+            final View view = viewList.remove(position);
+            notifyItemRemoved(position);
+            return view;
+        }
+
+        return null;
+    }
+
+    public void addView(@NonNull View view) {
+        addView(getItemCount(), view);
+    }
+
+    public void addViews(List<View> items) {
+        if (null == items || items.isEmpty()) return;
+
+        viewList.addAll(items);
+        notifyItemRangeInserted(getItemCount() - items.size(), items.size());
+    }
+
+    public void moveView(int fromPosition, int toPosition) {
+        final View view = viewList.remove(fromPosition);
+        viewList.add(toPosition, view);
+        notifyItemMoved(fromPosition, toPosition);
+    }
+
+    public List<View> getViews() {
+        return viewList;
+    }
+
+}
